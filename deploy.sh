39604881dd537fa8e93022967205e21ecca5a307
jojo-koomson <koomson.lm@gmail.com>
2024-02-19 11:44:27 +0000

    endpoint-update

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ npm run build
 
 echo "deploying files to server"
 
-scp -P 4422 -r dist/* githubuser@10.247.5.180:/var/www/smartfind
+scp -P 4422 -r dist/* githubuser@10.247.5.180:/var/www/smartfind/frontend
 
 echo "done!"
